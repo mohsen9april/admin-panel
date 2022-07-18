74d7a6b30b0e93632dfb4513138a56375ef3308e
Mohsen9april <Mohsen9april@gmail.com>
2022-07-18 12:45:13 +0430

    initial and create base of website

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='AppGlass'>
+        <div>one</div>
+        <div>two</div>
+        <div>three</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
