27e5e7dd6bba8d3042b3b871bab0b518a51d12ed
Mohsen9april <Mohsen9april@gmail.com>
2022-07-18 12:53:09 +0430

    create base webpage

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div className="App">
       <div className='AppGlass'>
-        <div>one</div>
-        <div>two</div>
-        <div>three</div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </div>
   );
